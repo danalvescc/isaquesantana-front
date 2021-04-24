edf3ff210e5c7186d325a95892a78b1ea2d3b00f
Daniel Alves <danalvescc@gmail.com>
2021-04-23 23:00:42 -0300

    Feat(all): Create header

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import { Header } from '../components/Header'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Isaque Santana</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;1,700;1,900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Antonio:wght@400;600&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;1,700;1,900&display=swap" rel="stylesheet" />
+      </Head>
+      <Header />
+    </div>
+  )
+}
